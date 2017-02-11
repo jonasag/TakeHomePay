ced96912e8730fd1055c60b9545c22437457e2a4
jonasag <jag@agertoft.net>
2017-02-11 17:38:57 +0000

    initital commit

---
+++ TakeHomePay.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TakeHomePay.Website.Startup))]
+namespace TakeHomePay.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
